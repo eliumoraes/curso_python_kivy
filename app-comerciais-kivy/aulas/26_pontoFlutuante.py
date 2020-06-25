61c13dddabd9ea201fc63ffbe51df821d6593069
Eliu Moraes <eliumoraes@gmail.com>
2020-06-25 20:45:23 -0300

    Primeiros envios

---
+++ app-comerciais-kivy/aulas/26_pontoFlutuante.py
@@ -0,0 +1,6 @@
+num_dec = 7.3
+
+
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.10f" %num_dec)
+print("Concatenando decimal: " +str(num_dec))
---
+++ app-comerciais-kivy/aulas/26_strings.py
@@ -0,0 +1,13 @@
+num_int = 5
+num_dec = 7.3
+val_str = "Qualquer texto"
+
+
+print("Concatenando decimal:", num_dec)
+print("Concatenando decimal: %.10f" %num_dec)
+print("Concatenando decimal: " +str(num_dec))
+
+
+print("Concatenando strings", val_str)
+print("Concatenando strings %s" %val_str)
+print("Concatenando strings " +val_str)
